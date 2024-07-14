f17dccb1d49269d86261c6371685e2af9aa791e0
user <elshanguliyev513@gmail.com>
2024-07-15 00:23:37 +0400

    create ECommerceProjectWithWebAPI in DataAccess/Concrete/Contexts

---
+++ DataAccess/Concrete/Contexts/ECommerceProjectWithWebAPIContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.Contexts
+{
+    public class ECommerceProjectWithWebAPIContext:DbContext
+    {
+        public ECommerceProjectWithWebAPIContext(DbContextOptions<ECommerceProjectWithWebAPIContext>options):base(options)
+        {
+                
+        }
+        public ECommerceProjectWithWebAPIContext()
+        {
+                
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connString = "Data Source=GULIYEV\\MSSQLSERVER2022;Initial Catalog=ECommerceProjectWithWebAPIDb;Integrated Security=true";
+            optionsBuilder.UseSqlServer(connString);
+        
+        }
+    }
+}
