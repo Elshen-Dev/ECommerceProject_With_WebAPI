83ca66b06073269f420a2fec3582fadbada63178
user <elshanguliyev513@gmail.com>
2024-07-14 21:16:33 +0400

    create UserAddDto in Dtos/UserDtos

---
+++ Entities/Dtos/UserDtos/UserAddDto.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Abstract;
+using System;
+
+namespace Entities.Dtos.UserDtos
+{
+    public class UserAddDto:IDto
+    {
+        public string Username { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Password { get; set; }
+        public bool Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
