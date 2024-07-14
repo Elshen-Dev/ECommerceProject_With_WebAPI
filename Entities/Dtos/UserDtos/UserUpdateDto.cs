b8fbd544826c25ccb2e3858b61d9042e87933a33
user <elshanguliyev513@gmail.com>
2024-07-14 21:17:11 +0400

    create UserUpdateDto in Dtos/UserDtos

---
+++ Entities/Dtos/UserDtos/UserUpdateDto.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Abstract;
+using System;
+
+namespace Entities.Dtos.UserDtos
+{
+    public class UserUpdateDto:IDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Password { get; set; }
+        public bool Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
